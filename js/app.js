9b541a348ab3e0046b28c6cb2d043e24493c180d
zaidnusair <zaid.nusair@hotmail.com>
2020-06-11 17:04:23 +0300

    updated nagman code, not completed yet

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 "use strict";
-lab2
-var sum = 0;
- var person = prompt("Hello, whats your name?");
- alert("Hello " + person + " my name is Zaid, welcome to my website!");
+// lab2;
+// var sum = 0;
+// var person = prompt("Hello, whats your name?");
+// alert("Hello " + person + " my name is Zaid, welcome to my website!");
 
 // var q1 = prompt("Am i above 25 years old?", "answer with Y or N");
 // var Q1 = q1.toUpperCase();
@@ -81,61 +81,61 @@ var sum = 0;
 
 //lab3
 
-myFunction1();
-var end = alert("it was nice to meet you " + person + " ,enjoy the website.");
-alert(sum);
+// myFunction1();
+// var end = alert("it was nice to meet you " + person + " ,enjoy the website.");
+// alert(sum);
 
-var person = prompt("Hello, whats your name?");
-alert("Hello " + person + " my name is Zaid, welcome to my website!");
+// var person = prompt("Hello, whats your name?");
+// alert("Hello " + person + " my name is Zaid, welcome to my website!");
 
-var sum = 0; //mentioned
-var arrayquestion = [
-  "Am i above 25 years old? ,answer with Y or N",
-  "Is the highest degree i have is a bachelors degree?",
-  "do i live in Irbid?,answer with Y or N",
-  "did i go to University of JUST?,answer with Y or N",
-  "Can i play an instrument?,answer with Y or N",
-];
-for (var i = 0; i < arrayquestion.length; i++) {
-  var userInput = prompt(arrayquestion[i].toUpperCase());
-  condition(userInput);
-  function condition(userInput) {
-    //userInput = x
-    while (userInput != "Y" && userInput != "N") {
-      userInput = prompt(arrayquestion[i]).toUpperCase();
-    }
-    if (userInput == "Y") {
-      sum++;
-    }
-    return userInput;
-  }
-}
+// var sum = 0; //mentioned
+// var arrayquestion = [
+//   "Am i above 25 years old? ,answer with Y or N",
+//   "Is the highest degree i have is a bachelors degree?",
+//   "do i live in Irbid?,answer with Y or N",
+//   "did i go to University of JUST?,answer with Y or N",
+//   "Can i play an instrument?,answer with Y or N",
+// ];
+// for (var i = 0; i < arrayquestion.length; i++) {
+//   var userInput = prompt(arrayquestion[i].toUpperCase());
+//   condition(userInput);
+//   function condition(userInput) {
+//     //userInput = x
+//     while (userInput != "Y" && userInput != "N") {
+//       userInput = prompt(arrayquestion[i]).toUpperCase();
+//     }
+//     if (userInput == "Y") {
+//       sum++;
+//     }
+//     return userInput;
+//   }
+// }
 
-function question6() {
-  var q6 = prompt("how many strings does a guitar have?", "insert a number ");
-  var i;
-  for (i = 0; i <= 3; i++) {
-    if (q6 > 6) {
-      alert("lower");
-      var q6 = prompt(
-        "how many strings does a guitar have?",
-        "insert a number "
-      );
-    } else if (q6 < 6) {
-      alert("higher");
-      var q6 = prompt(
-        "how many strings does a guitar have?",
-        "insert a number "
-      );
-    } else if (q6 == 6) {
-      alert("correct! a guitar has 6 strings");
-      sum = sum + 1;
-      break;
-    }
-  }
-}
-question6();
-alert(" your grade is " + sum + " out of 7");
+// function question6() {
+//   var q6 = prompt("how many strings does a guitar have?", "insert a number ");
+//   var i;
+//   for (i = 0; i <= 3; i++) {
+//     if (q6 > 6) {
+//       alert("lower");
+//       var q6 = prompt(
+//         "how many strings does a guitar have?",
+//         "insert a number "
+//       );
+//     } else if (q6 < 6) {
+//       alert("higher");
+//       var q6 = prompt(
+//         "how many strings does a guitar have?",
+//         "insert a number "
+//       );
+//     } else if (q6 == 6) {
+//       alert("correct! a guitar has 6 strings");
+//       sum = sum + 1;
+//       break;
+//     }
+//   }
+// }
+// question6();
+// alert(" your grade is " + sum + " out of 7");
 
 // drawings:
 // alert("|----|\nO    |\n/|\\   |\n/ \\   |"); //final
@@ -195,7 +195,6 @@ alert(" your grade is " + sum + " out of 7");
 //   return ans;
 // }
 
-
 // for (var i= 0 ; i > 7){
 //   hungman();
 //   ans()
@@ -203,83 +202,93 @@ alert(" your grade is " + sum + " out of 7");
 
 // }
 
-  // "l" && "u" && "m" && "i" && "n" && "s"
-  // l || u || m || i || n || s  
-  // "_ _ _ _ _ _ _"
-  var v 
-var p = 7
+// "l" && "u" && "m" && "i" && "n" && "s"
+// l || u || m || i || n || s
+// "_ _ _ _ _ _ _"
+var v = ""; //drawing alerted
+var p = 6; //letters left
+var y = 0; //number of wrong answers
+var x = ""; //
 
-var q7= alert("lets play hangman!\n1 word, 6 letters. its a place you know","guess a letter").toLowerCase()
-
-ask();
-function ask(){
-  if (q7 == "l" ||
-q7 == "u" ||
-q7 == "m" ||
-q7 == "i" ||
-q7 == "n" ||
-q7 == "s"){
-
-
-  
-  p = p-1
-  progress()
-  alert("correct", x)
-
-  CORRECT()
- 
-}else{
-  y=y+1
-  INCORRECT()
+var q7 = prompt(
+  "lets play hangman!\n1 word, 7 letters. its a place you know",
+  "guess a letter"
+);
+// .toLowerCase();
+while (p > 0 && y < 6) {
+  ask();
 }
-}
+//this function increases the progress and alerts the user with the result
+function ask() {
+  if (
+    q7 == "l" ||
+    q7 == "u" ||
+    q7 == "m" ||
+    q7 == "i" ||
+    q7 == "n" ||
+    q7 == "s"
+  ) {
+    p = p - 1;
+    progress();
+    alert("correct", x);
 
-function CORRECT(){
-  do {
+    CORRECT();
+  } else {
+    y = y + 1;
+    INCORRECT();
+  }
+}
+// this function keeps asking the player the question until they guess all the letters
+function CORRECT() {
+  while (0 < p && p < 8) {
     ask();
-
-  }while (0 < p , p < 8  );
-
+    break;
   }
-
-function progress(){
-  for ( 0 < p , p < 7  ) {
-  x = (p , " letters left")
-  return x
-  }
-
-
-   
 }
 
-function INCORRECT(){
-  for (var y= 0 ; y <=6 ) {
-    drawing(y)
-    alert(v)
+//this function assigns to x the number of letters left to win
+function progress() {
+  while (0 < p < 7) {
+    x = (p, " letters left");
+    return x;
   }
+}
+// this function increases the counter for number of incorrect answer and alerts the player with a drawing
+function INCORRECT() {
+  while (y <= 6) {
+    drawing(y);
+    alert(v);
 
+    break;
+  }
 }
 
-function drawing(){
-  
-  if (y=1){
-    v = alert("|----|\nO    |\n       |\n       |");} //1st
-    else if(y=2){
-    v = alert("|----|\nO    |\n/     |\n      |");} //2nd
-    else if(y=3){
-    v = alert("|----|\nO    |\n/|    |\n      |");} //3rd
-    else if (y=4){
-    v = alert("|----|\nO    |\n/|\\   |\n     |");}//4rth
-    else if (y=5){
-    v = alert("|----|\nO    |\n/|\\   |\n/     |");} //5th
-    else if (y=6){
-    v = alert("|----|\nO    |\n/|\\   |\n/ \\   |");} //6th
+//this function gives a new drawing each time you make a mistake
+function drawing() {
+  if ((y = 1)) {
+    v = "|----|\nO    |\n       |\n       |";
+  } //1st
+  else if ((y = 2)) {
+    v = "|----|\nO    |\n/     |\n      |";
+  } //2nd
+  else if ((y = 3)) {
+    v = "|----|\nO    |\n/|    |\n      |";
+  } //3rd
+  else if ((y = 4)) {
+    v = "|----|\nO    |\n/|\\   |\n     |";
+  } //4rth
+  else if ((y = 5)) {
+    v = "|----|\nO    |\n/|\\   |\n/     |";
+  } //5th
+  else if ((y = 6)) {
+    v = "|----|\nO    |\n/|\\   |\n/ \\   |";
+  } //6th
 
-return v
+  return v;
 }
-
-if (x<1){
-  alert("you won, the answer is lumnius")
-}else if(y=6){
-  alert("you lost, the answer is luminus")
+//result
+if (x < 1) {
+  alert("you won, the answer is lumnius");
+} else if ((y = 6)) {
+  alert("you lost, the answer is luminus");
 }
